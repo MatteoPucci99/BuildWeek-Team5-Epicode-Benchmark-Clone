38ec03dd3807da59f9b01c5159348f4c1eeb762d
Carlo Vitetta <143413832+Carlovite@users.noreply.github.com>
2023-09-20 12:01:02 +0200

    timer new

--- resultProgress/resultCIrcleNew.js
+++ resultProgress/resultCIrcleNew.js
@@ -1,23 +1,23 @@
 let circularProgression = document.querySelector(".circular-progress");
 let progressValue = document.querySelector(".progress-value");
 
-let progressStartValue = 0,
-  progressEndValue = 70,
-  speed = 20;
-if (progressEndValue == 0) {
-  circularProgression.style.background = "blue";
-  progressValue.textContent = `${progressStartValue}%`;
+let progressStartValue = 60,
+  progressEndValue = 0,
+  speed = 1000;
+if (progressStartValue === 0) {
+  // circularProgression.style.background = "blue";
+  progressValue.textContent = `${progressStartValue}`;
 } else {
   const progress = setInterval(() => {
-    progressStartValue++;
+    progressStartValue--;
 
-    progressValue.textContent = `${progressStartValue}%`;
-    circularProgression.style.background = `conic-gradient(hsl(318.07deg 83.02% 41.57%) ${
-      progressStartValue * 3.6
-    }deg, hsl(180deg 100% 50%) 0deg)`;
+    progressValue.textContent = `${progressStartValue}`;
+    circularProgression.style.background = `conic-gradient(hsl(180deg 100% 50%) 
+    ${progressStartValue * 6}deg, hsl(0, 0%, 87%) 0deg)`;
 
     if (progressStartValue == progressEndValue) {
       clearInterval(progress);
     }
   }, speed);
 }
+// hsl(180deg 100% 50%)
--- timer-prototype/script.js
+++ timer-prototype/script.js
@@ -1,11 +1,23 @@
 let number = document.getElementsByTagName("h2")[0];
-let counter = 60;
+let counter = 30;
+// setInterval(() => {
+//   if (counter === 0) {
+//     // clearInterval();
+//     // number.innerText = "TEMPO SCADUTO";
+//   } else {
+//     counter -= 1;
+//     number.innerText = counter;
+//   }
+// }, 1000);
+
 setInterval(() => {
-  if (counter === 0) {
-    clearInterval();
-    // number.innerText = "TEMPO SCADUTO";
-  } else {
+  if (counter !== 0) {
     counter -= 1;
     number.innerText = counter;
+
+    // clearInterval();
+    // number.innerText = "TEMPO SCADUTO";
+  } else {
+    counter = 30;
   }
 }, 1000);
